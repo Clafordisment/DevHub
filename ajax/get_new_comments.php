7f8fd7842d67784e8c0f29c2deb4ea206465a371
Clafordisment <shelzy645@gmail.com>
2026-04-09 17:06:02 +0300

    Correcting typos in the executable PHP polling file

--- ajax/get_new_comments.php
+++ ajax/get_new_comments.php
@@ -8,7 +8,7 @@ header('Content-Type: application/json');
 $postId = isset($_GET['post_id']) ? (int)$_GET['post_id'] : 0;
 $lastId = isset($_GET['last_id']) ? (int)$_GET['last_id'] : 0;
 
-if(postId <= 0)
+if($postId <= 0)
     {
         echo json_encode(['comments' => []]);
         exit;
@@ -44,16 +44,16 @@ if ($result)
         while ($row = $result->fetch_assoc())
             {
                 $comments[] = [
-                                'id' => (int)$row['id_c'],
-            'content' => $row['content'],
-            'author' => $row['comment_author'],
-            'author_id' => (int)$row['author_id'],
-            'likes_count' => (int)$row['likes_count'],
-            'date' => date('d.m.Y H:i', strtotime($row['created_at']))
+                    'id' => (int)$row['id_c'],
+                    'content' => $row['content'],
+                    'author' => $row['comment_author'],
+                    'author_id' => (int)$row['author_id'],
+                    'likes_count' => (int)$row['likes_count'],
+                    'date' => date('d.m.Y H:i', strtotime($row['created_at']))
                 ]
             }
     }
 
-    echo json_encode(['comments' => []]);
+    echo json_encode(['comments'] => []);
     $conn->close();
 ?>
